2ed3c4a14a0a457589bf60b966219428f5fe9904
kujira <ironys@foxmail.com>
2022-12-01 15:15:45 +0800

    fix: 更改为https请求

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,7 @@ export default {
   tailwindcss: {},
   plugins: ["@umijs/plugins/dist/tailwindcss"],
   title: 'Sango上传工具',
-  favicons: ["http://cdn.meiqijiacheng.com/h5-deploy/favicon.ico"],
+  favicons: ["https://cdn.meiqijiacheng.com/h5-deploy/favicon.ico"],
   // Git Page相关配置
   base: process.env.NODE_ENV === 'production' ? '/SangoUploader/' : '/',
   publicPath: process.env.NODE_ENV === 'production' ? '/SangoUploader/' : '/',
